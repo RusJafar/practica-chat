af13a66b46fe3b433f09049a34520f0c7df923fe
jafartafary <jafartafary@gmail.com>
2022-08-21 22:58:49 +0300

    Added profile page

--- src/components/singleChatMessageInput/singleChatMessageInput.tmpl.js
+++ src/components/singleChatMessageInput/singleChatMessageInput.tmpl.js
@@ -3,8 +3,8 @@ export default `
     <div class="chat-message__attach-button">
         <div onclick={alert('Hello')} class="attach-button">&#43;</div>
     </div>
-    <div class="chat-message__input">
-        <input class="message-input" type="text" placeholder="message"/>
+    <div class="chat-message__input"
+        <input class="message-input" type="text" name="massege" placeholder="message"/>
     </div>
     <div class="chat-message__submit">
         <div onclick={alert('Hello')} class="message-submit-button" type="button">
